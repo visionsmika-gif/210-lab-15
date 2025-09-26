6683290a3e1d6da8f43ef3d89839fb1e249b2e6a
visionsmika-gif <visionsmika@gmail.com>
2025-09-26 15:28:21 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,5 @@
+// Code a Movie class that has the screen writer, the year released, and the title as its private member variables. It has the standard setters and getters for each private member variable. Also code a print() method which prints the object data in a simple format.
+// Your code should read data from an input file, using the data below, which lists data in this order: title, year released, screen writer name.
+// Read this data into a temporary Movie object.Then append that object to your container.
+// For your container, you can choose an <array> class array or a <vector> class vector.Store your four records in this container.
+// Towards the end of your main() function, output the contents of the array / vector.
